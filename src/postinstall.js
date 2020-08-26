38c550128070e4ecec8270eba342e62473d458f7
Corey Phillips <corey.lyle.phillips@protonmail.com>
2020-08-26 15:45:44 -0400

    Updated postinstall.js to properly handle iOS.

--- src/postinstall.js
+++ src/postinstall.js
@@ -1,22 +1,19 @@
-var exec = require("child_process").exec;
-var os = require("os");
-
-const generalCmd = exec(
-	"rn-nodeify --install buffer,stream,assert,events,crypto,vm,process --hack"
-);
+var exec = require('child_process').exec;
+var os = require('os');
 
 function postInstallMac() {
-	exec(`${generalCmd}`);
+	exec(
+		'rn-nodeify --install buffer,stream,assert,events,crypto,vm,process --hack && cd nodejs-assets/nodejs-project && yarn install && cd ../../ios && pod install && cd ..',
+	);
 }
 function postInstallLinWin() {
-	exec(`${generalCmd}`);
+	exec(
+		'rn-nodeify --install buffer,stream,assert,events,crypto,vm,process --hack && cd nodejs-assets/nodejs-project && yarn install && cd ../..',
+	);
 }
 
-if (os.type() === "Linux")
-	postInstallLinWin();
-else if (os.type() === "Windows_NT")
-	postInstallLinWin();
-else if (os.type() === "Darwin")
+if (os.type() === 'Darwin') {
 	postInstallMac();
-else
-	throw new Error("Unsupported OS found: " + os.type());
+} else {
+	postInstallLinWin();
+}
