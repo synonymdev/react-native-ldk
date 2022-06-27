31d3e293116eef3d602c41680b890db69d439610
Jason van den Berg <jayvdb1@gmail.com>
2022-06-27 18:43:42 +0200

    Revert "WIP"
    This reverts commit 989324778168fb22d12fa24b02b41dac95a6d481.

--- example/App.tsx
+++ example/App.tsx
@@ -8,8 +8,8 @@ import {
 	StyleSheet,
 	Text,
 } from 'react-native';
-// import lm from '@synonymdev/react-native-ldk';
-// import ldk from '@synonymdev/react-native-ldk/dist/ldk'; //For direct access to LDK functions
+import lm from '@synonymdev/react-native-ldk';
+import ldk from '@synonymdev/react-native-ldk/dist/ldk'; //For direct access to LDK functions
 
 //const testNodePubkey = '034ecfd567a64f06742ac300a2985676abc0b1dc6345904a08bb52d5418e685f79';
 //const testNodeAddress = '35.240.72.95';
@@ -33,14 +33,14 @@ const App = () => {
 						onPress={async () => {
 							setMessage('Starting LDK...');
 							try {
-								// const res = await lm.start({});
-								//
-								// if (res.isErr()) {
-								// 	setMessage(res.error.message);
-								// 	return;
-								// }
-								//
-								// setMessage(JSON.stringify(res.value));
+								const res = await lm.start({});
+
+								if (res.isErr()) {
+									setMessage(res.error.message);
+									return;
+								}
+
+								setMessage(JSON.stringify(res.value));
 							} catch (e) {
 								console.error(e);
 								setMessage(e.toString());
@@ -48,187 +48,187 @@ const App = () => {
 						}}
 					/>
 
-					{/*<Button*/}
-					{/*	title={'Add peer'}*/}
-					{/*	onPress={async () => {*/}
-					{/*		setMessage('Adding peer...');*/}
-					{/*		try {*/}
-					{/*			const res = await ldk.addPeer({*/}
-					{/*				pubKey:*/}
-					{/*					'02b96088e17dd53a4297d0769e8166a64c25ff47b143cbd6615723cc2a49efbb65',*/}
-					{/*				address: '10.0.0.103',*/}
-					{/*				port: 9837,*/}
-					{/*				timeout: 5000,*/}
-					{/*			});*/}
+					<Button
+						title={'Add peer'}
+						onPress={async () => {
+							setMessage('Adding peer...');
+							try {
+								const res = await ldk.addPeer({
+									pubKey:
+										'02b96088e17dd53a4297d0769e8166a64c25ff47b143cbd6615723cc2a49efbb65',
+									address: '10.0.0.103',
+									port: 9837,
+									timeout: 5000,
+								});
 
-					{/*			if (res.isErr()) {*/}
-					{/*				setMessage(res.error.message);*/}
-					{/*				return;*/}
-					{/*			}*/}
+								if (res.isErr()) {
+									setMessage(res.error.message);
+									return;
+								}
 
-					{/*			setMessage(JSON.stringify(res.value));*/}
-					{/*		} catch (e) {*/}
-					{/*			setMessage(e.toString());*/}
-					{/*		}*/}
-					{/*	}}*/}
-					{/*/>*/}
+								setMessage(JSON.stringify(res.value));
+							} catch (e) {
+								setMessage(e.toString());
+							}
+						}}
+					/>
 
-					{/*<Button*/}
-					{/*	title={'List peers'}*/}
-					{/*	onPress={async () => {*/}
-					{/*		try {*/}
-					{/*			const res = await ldk.listPeers();*/}
+					<Button
+						title={'List peers'}
+						onPress={async () => {
+							try {
+								const res = await ldk.listPeers();
 
-					{/*			if (res.isErr()) {*/}
-					{/*				setMessage(res.error.message);*/}
-					{/*				return;*/}
-					{/*			}*/}
+								if (res.isErr()) {
+									setMessage(res.error.message);
+									return;
+								}
 
-					{/*			setMessage(JSON.stringify(res.value));*/}
-					{/*		} catch (e) {*/}
-					{/*			setMessage(e.toString());*/}
-					{/*		}*/}
-					{/*	}}*/}
-					{/*/>*/}
+								setMessage(JSON.stringify(res.value));
+							} catch (e) {
+								setMessage(e.toString());
+							}
+						}}
+					/>
 
-					{/*<Button*/}
-					{/*	title={'List channels'}*/}
-					{/*	onPress={async () => {*/}
-					{/*		try {*/}
-					{/*			const res = await ldk.listChannels();*/}
+					<Button
+						title={'List channels'}
+						onPress={async () => {
+							try {
+								const res = await ldk.listChannels();
 
-					{/*			if (res.isErr()) {*/}
-					{/*				setMessage(res.error.message);*/}
-					{/*				return;*/}
-					{/*			}*/}
+								if (res.isErr()) {
+									setMessage(res.error.message);
+									return;
+								}
 
-					{/*			let msg = '';*/}
-					{/*			res.value.forEach((channel) => {*/}
-					{/*				Object.keys(channel).forEach((key) => {*/}
-					{/*					// @ts-ignore*/}
-					{/*					msg += `${key}: ${channel[key]}\n`;*/}
-					{/*				});*/}
-					{/*			});*/}
+								let msg = '';
+								res.value.forEach((channel) => {
+									Object.keys(channel).forEach((key) => {
+										// @ts-ignore
+										msg += `${key}: ${channel[key]}\n`;
+									});
+								});
 
-					{/*			setMessage(msg);*/}
-					{/*		} catch (e) {*/}
-					{/*			setMessage(e.toString());*/}
-					{/*		}*/}
-					{/*	}}*/}
-					{/*/>*/}
+								setMessage(msg);
+							} catch (e) {
+								setMessage(e.toString());
+							}
+						}}
+					/>
 
-					{/*<Button*/}
-					{/*	title={'List usable channels'}*/}
-					{/*	onPress={async () => {*/}
-					{/*		try {*/}
-					{/*			const res = await ldk.listUsableChannels();*/}
+					<Button
+						title={'List usable channels'}
+						onPress={async () => {
+							try {
+								const res = await ldk.listUsableChannels();
 
-					{/*			if (res.isErr()) {*/}
-					{/*				setMessage(res.error.message);*/}
-					{/*				return;*/}
-					{/*			}*/}
+								if (res.isErr()) {
+									setMessage(res.error.message);
+									return;
+								}
 
-					{/*			let msg = '';*/}
-					{/*			res.value.forEach((channel) => {*/}
-					{/*				Object.keys(channel).forEach((key) => {*/}
-					{/*					// @ts-ignore*/}
-					{/*					msg += `${key}: ${channel[key]}\n`;*/}
-					{/*				});*/}
-					{/*			});*/}
+								let msg = '';
+								res.value.forEach((channel) => {
+									Object.keys(channel).forEach((key) => {
+										// @ts-ignore
+										msg += `${key}: ${channel[key]}\n`;
+									});
+								});
 
-					{/*			setMessage(msg);*/}
-					{/*		} catch (e) {*/}
-					{/*			setMessage(e.toString());*/}
-					{/*		}*/}
-					{/*	}}*/}
-					{/*/>*/}
+								setMessage(msg);
+							} catch (e) {
+								setMessage(e.toString());
+							}
+						}}
+					/>
 
-					{/*<Button*/}
-					{/*	title={'Pay invoice'}*/}
-					{/*	onPress={async () => {*/}
-					{/*		const paymentRequest =*/}
-					{/*			'lnbcrt100u1p3gw4lmpp5g8nn2m856gwmc2rxlsgacw9746tyrzz5nh8svq6s6usmqqwdkn9qdp92phkcctjypykuan0d93k2grxdaezqcmpwfhkcxqyjw5qcqp2sp5csqwsteyyfeg6l0pfa6x6cwh88aklqzy0g7s5ndrg55h5fanwajq9qy9qsqtvfuzpl3w00tfvfgwt0waxsl9fe4z6eu4cwttulx5c5p5fl442vynngr9w0un3janvcr68a3vtlyn3js0j332wzdry5wneat6f2365gp6xstn6';*/}
-					{/*		const res = await ldk.decode({ paymentRequest });*/}
-					{/*		if (res.isErr()) {*/}
-					{/*			return setMessage(res.error.message);*/}
-					{/*		}*/}
+					<Button
+						title={'Pay invoice'}
+						onPress={async () => {
+							const paymentRequest =
+								'lnbcrt100u1p3gw4lmpp5g8nn2m856gwmc2rxlsgacw9746tyrzz5nh8svq6s6usmqqwdkn9qdp92phkcctjypykuan0d93k2grxdaezqcmpwfhkcxqyjw5qcqp2sp5csqwsteyyfeg6l0pfa6x6cwh88aklqzy0g7s5ndrg55h5fanwajq9qy9qsqtvfuzpl3w00tfvfgwt0waxsl9fe4z6eu4cwttulx5c5p5fl442vynngr9w0un3janvcr68a3vtlyn3js0j332wzdry5wneat6f2365gp6xstn6';
+							const res = await ldk.decode({ paymentRequest });
+							if (res.isErr()) {
+								return setMessage(res.error.message);
+							}
 
-					{/*		const { recover_payee_pub_key, amount_milli_satoshis } =*/}
-					{/*			res.value;*/}
+							const { recover_payee_pub_key, amount_milli_satoshis } =
+								res.value;
 
-					{/*		Alert.alert(*/}
-					{/*			`Pay ${amount_milli_satoshis ?? 0}`,*/}
-					{/*			`To pubkey: ${recover_payee_pub_key}`,*/}
-					{/*			[*/}
-					{/*				{*/}
-					{/*					text: 'Cancel',*/}
-					{/*					onPress: () => console.log('Cancel Pressed'),*/}
-					{/*					style: 'cancel',*/}
-					{/*				},*/}
-					{/*				{*/}
-					{/*					text: 'Pay',*/}
-					{/*					onPress: async () => {*/}
-					{/*						const pay = await ldk.pay({ paymentRequest });*/}
-					{/*						if (pay.isErr()) {*/}
-					{/*							return setMessage(pay.error.message);*/}
-					{/*						}*/}
+							Alert.alert(
+								`Pay ${amount_milli_satoshis ?? 0}`,
+								`To pubkey: ${recover_payee_pub_key}`,
+								[
+									{
+										text: 'Cancel',
+										onPress: () => console.log('Cancel Pressed'),
+										style: 'cancel',
+									},
+									{
+										text: 'Pay',
+										onPress: async () => {
+											const pay = await ldk.pay({ paymentRequest });
+											if (pay.isErr()) {
+												return setMessage(pay.error.message);
+											}
 
-					{/*						setMessage(pay.value);*/}
-					{/*					},*/}
-					{/*				},*/}
-					{/*			],*/}
-					{/*		);*/}
-					{/*	}}*/}
-					{/*/>*/}
+											setMessage(pay.value);
+										},
+									},
+								],
+							);
+						}}
+					/>
 
-					{/*<Button*/}
-					{/*	title={'Create invoice'}*/}
-					{/*	onPress={async () => {*/}
-					{/*		try {*/}
-					{/*			const res = await ldk.createPaymentRequest({*/}
-					{/*				amountSats: 1234,*/}
-					{/*				description: 'paymeplz',*/}
-					{/*			});*/}
+					<Button
+						title={'Create invoice'}
+						onPress={async () => {
+							try {
+								const res = await ldk.createPaymentRequest({
+									amountSats: 1234,
+									description: 'paymeplz',
+								});
 
-					{/*			if (res.isErr()) {*/}
-					{/*				setMessage(res.error.message);*/}
-					{/*				return;*/}
-					{/*			}*/}
+								if (res.isErr()) {
+									setMessage(res.error.message);
+									return;
+								}
 
-					{/*			const { to_str } = res.value;*/}
+								const { to_str } = res.value;
 
-					{/*			setMessage(to_str);*/}
-					{/*		} catch (e) {*/}
-					{/*			setMessage(e.toString());*/}
-					{/*		}*/}
-					{/*	}}*/}
-					{/*/>*/}
+								setMessage(to_str);
+							} catch (e) {
+								setMessage(e.toString());
+							}
+						}}
+					/>
 
-					{/*<Button*/}
-					{/*	title={'Get info'}*/}
-					{/*	onPress={async () => {*/}
-					{/*		const nodeIdRes = await ldk.nodeId();*/}
-					{/*		if (nodeIdRes.isErr()) {*/}
-					{/*			return setMessage(nodeIdRes.error.message);*/}
-					{/*		}*/}
+					<Button
+						title={'Get info'}
+						onPress={async () => {
+							const nodeIdRes = await ldk.nodeId();
+							if (nodeIdRes.isErr()) {
+								return setMessage(nodeIdRes.error.message);
+							}
 
-					{/*		console.log(nodeIdRes.value);*/}
+							console.log(nodeIdRes.value);
 
-					{/*		setMessage(`Node ID: ${nodeIdRes.value}`);*/}
-					{/*	}}*/}
-					{/*/>*/}
+							setMessage(`Node ID: ${nodeIdRes.value}`);
+						}}
+					/>
 
-					{/*<Button*/}
-					{/*	title={'Show version'}*/}
-					{/*	onPress={async () => {*/}
-					{/*		const res = await ldk.version();*/}
-					{/*		if (res.isErr()) {*/}
-					{/*			return setMessage(res.error.message);*/}
-					{/*		}*/}
+					<Button
+						title={'Show version'}
+						onPress={async () => {
+							const res = await ldk.version();
+							if (res.isErr()) {
+								return setMessage(res.error.message);
+							}
 
-					{/*		setMessage(res.value.ldk);*/}
-					{/*	}}*/}
-					{/*/>*/}
+							setMessage(res.value.ldk);
+						}}
+					/>
 				</ScrollView>
 			</SafeAreaView>
 		</>
