65409c39b496cc743553c1e17f1e165b53e1ae48
Jason van den Berg <jayvdb1@gmail.com>
2022-07-14 16:19:38 +0200

    Swift write LDK logs to files

--- lib/ios/Classes/LdkLogger.swift
+++ lib/ios/Classes/LdkLogger.swift
@@ -21,11 +21,40 @@ class LdkLogger: Logger {
         //Only when the JS code has set the log level to active
         if activeLevels[level] == true {
             LdkEventEmitter.shared.send(withEvent: .ldk_log, body: record.get_args())
+            Logfile.log.write(record.get_args())
         }
+        
     }
     
     func setLevel(level: UInt32, active: Bool) {
         self.activeLevels[level] = active
         LdkEventEmitter.shared.send(withEvent: .native_log, body: "Log level \(level) set to \(active)")
     }
+}
+
+class Logfile: TextOutputStream {
+    lazy var logfile: URL = {
+        //Each session has it's own log file
+        let f = DateFormatter()
+        f.dateFormat = "d-M-y-HH-mm-ss"
+
+        let log = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("ldk-\(f.string(from: Date())).txt")
+        print("Log path: \(log.path)")
+
+        return log
+    }()
+    
+    func write(_ str: String) {
+        let line = "\(str)\n"
+        
+        if let handle = try? FileHandle(forWritingTo: logfile) {
+            handle.seekToEndOfFile()
+            handle.write(line.data(using: .utf8)!)
+            handle.closeFile()
+        } else {
+            try? line.data(using: .utf8)?.write(to: logfile)
+        }
+    }
+    static var log = Logfile()
+    private init() {}
 }
