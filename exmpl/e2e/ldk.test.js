d5592822cd9652f3098ece11ab1793fb67f52005
Ivan Vershigora <ivan.vershigora@gmail.com>
2022-12-05 22:42:16 +0300

    new exmpl project

---
+++ exmpl/e2e/ldk.test.js
@@ -0,0 +1,28 @@
+describe('LDK integration test', () => {
+	beforeAll(async () => {
+		await device.launchApp();
+	});
+
+	beforeEach(async () => {
+		await device.reloadReactNative();
+	});
+
+	it('should have heading', async () => {
+		await waitFor(element(by.text('react-native-ldk')))
+			.toBeVisible()
+			.withTimeout(20000);
+	});
+
+	it('should show "Running LDK" after starting up', async () => {
+		// await element(by.id('start')).tap();
+		await waitFor(element(by.text('Running LDK')))
+			.toBeVisible()
+			.withTimeout(20000);
+
+		await element(by.id('E2ETest')).tap();
+
+		await waitFor(element(by.text('e2e success')))
+			.toBeVisible()
+			.withTimeout(20000);
+	});
+});
---
+++ exmpl/utils/result.ts
@@ -0,0 +1,46 @@
+export type Result<T> = Ok<T> | Err<T>;
+
+export class Ok<T> {
+	public constructor(public readonly value: T) {}
+
+	public isOk(): this is Ok<T> {
+		return true;
+	}
+
+	public isErr(): this is Err<T> {
+		return false;
+	}
+}
+
+export class Err<T> {
+	public constructor(public readonly error: Error) {
+		//Don't console log for unit tests or if we're not in dev mode
+		if (process.env.JEST_WORKER_ID === undefined && __DEV__) {
+			console.info(error);
+		}
+	}
+
+	public isOk(): this is Ok<T> {
+		return false;
+	}
+
+	public isErr(): this is Err<T> {
+		return true;
+	}
+}
+
+/**
+ * Construct a new Ok result value.
+ */
+export const ok = <T>(value: T): Ok<T> => new Ok(value);
+
+/**
+ * Construct a new Err result value.
+ */
+export const err = <T>(error: Error | string): Err<T> => {
+	if (typeof error === 'string') {
+		return new Err(new Error(error));
+	}
+
+	return new Err(error);
+};
