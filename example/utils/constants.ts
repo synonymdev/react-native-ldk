35b94b2d6857edf5740640b9ff1297766c2f186a
Collin Rukundo <collin@matchstick.ug>
2023-03-20 15:23:08 +0300

    Add voltage lsp as peer for -conf channel and switch to testnet

--- example/utils/constants.ts
+++ example/utils/constants.ts
@@ -1,33 +1,42 @@
 import { TAvailableNetworks } from '@synonymdev/react-native-ldk';
 
-export const selectedNetwork: TAvailableNetworks = 'bitcoinRegtest';
+export const selectedNetwork: TAvailableNetworks = 'bitcoinTestnet';
 
 //Lightning Peer Info
 export const peers = {
-	lnd: {
+	// //lnd
+	// alice: {
+	// 	pubKey:
+	// 		'037b7977cb2db3bd27a2b1c8811e86f43e2ad8918fa942169cb5196c05034d4206',
+	// 	address: '127.0.0.1',
+	// 	port: 9735,
+	// },
+	// // lnd
+	// bob: {
+	// 	pubKey:
+	// 		'03c3550c98eed2e435dea0b64457c628ecb087c9e5d819b85054beff5146c0bddc',
+	// 	address: '127.0.0.1',
+	// 	port: 9736,
+	// },
+	// voltage lsp for zero-conf channel
+	lsp: {
 		pubKey:
-			'0239af55c86cf84167bc216ffa48687b304a4f0601d6496febf8bbb286a65c814c',
-		address: '127.0.0.1',
+			'025804d4431ad05b06a1a1ee41f22fefeb8ce800b0be3a92ff3b9f594a263da34e',
+		address: '44.228.24.253',
 		port: 9735,
-	},
-	clightning: {
-		pubKey:
-			'03e8fa0ceb4aa0cc696278189269e01bac22b0549ad62fac726f26cc6c65008ef4',
-		address: '127.0.0.1',
-		port: 9836,
-	},
-	eclair: {
-		pubKey:
-			'03e0588a30b5db6eb91df62a243e179b11f63fe27c44856e510055a9156a845bd2',
-		address: '127.0.0.1',
-		port: 9937,
 	},
 };
 
-//Electrum Server Info (Synonym Regtest Set By Default)
 export const customPeers = {
 	bitcoin: [],
-	bitcoinTestnet: [],
+	bitcoinTestnet: [
+		{
+			host: 'testnet.aranguren.org',
+			ssl: 51002,
+			tcp: 51001,
+			protocol: 'tcp',
+		},
+	],
 	bitcoinRegtest: [
 		{
 			host: '35.233.47.252',
