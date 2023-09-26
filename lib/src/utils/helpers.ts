6a7d814646c80a02a7eb191635ee8309d2e2be12
Corey Phillips <corey.lyle.phillips@proton.me>
2023-09-26 07:42:17 -0400

    fix(lightning): syncLdk Updates
    Prevents syncLdk from having overlapping calls, instead returning the outcome of the current sync.
    Adds retry attempts to syncLdk if a network query times out.

--- lib/src/utils/helpers.ts
+++ lib/src/utils/helpers.ts
@@ -296,3 +296,14 @@ export const findOutputsFromRawTxs = (
 
 	return result;
 };
+
+/**
+ * Pauses execution of a function.
+ * @param {number} ms The time to wait in milliseconds.
+ * @returns {Promise<void>}
+ */
+export const sleep = (ms = 1000): Promise<void> => {
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms);
+	});
+};
