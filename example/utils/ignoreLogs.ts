b69ff1e90c6197af8a99f0b4bcd404ace4403fb5
Ovi Trif <ovitrif@proton.me>
2024-02-13 20:05:57 +0100

    chore(example): add support to disable ldk logs

---
+++ example/utils/ignoreLogs.ts
@@ -0,0 +1,44 @@
+import { LogBox } from 'react-native';
+
+const ENABLE_LOGBOX = true;
+const ENABLE_LDK_LOGS = true;
+
+if (__DEV__) {
+	const ignoredLogs: string[] = [];
+	const ignoredInfo: string[] = [];
+	const ignoredWarnings: string[] = [];
+	const ignoredErrors: string[] = [];
+
+	if (!ENABLE_LOGBOX) {
+		LogBox.ignoreAllLogs();
+	}
+
+	if (!ENABLE_LDK_LOGS) {
+		ignoredLogs.push('LDK:', 'react-native-ldk:', 'DEBUG (JS)', 'ERROR (JS)');
+	}
+
+	const withoutIgnored = (
+		logger: (...data: any[]) => void,
+		ignoreList: string[],
+	): any => {
+		return (...args): void => {
+			let output: string;
+			try {
+				output = args.join(' ');
+			} catch (err) {
+				// if we can't check if the log should be ignored, just log it
+				logger(...args);
+				return;
+			}
+
+			if (!ignoreList.some((log) => output.includes(log))) {
+				logger(...args);
+			}
+		};
+	};
+
+	console.log = withoutIgnored(console.log, ignoredLogs);
+	console.info = withoutIgnored(console.info, ignoredInfo);
+	console.warn = withoutIgnored(console.warn, ignoredWarnings);
+	console.error = withoutIgnored(console.error, ignoredErrors);
+}
